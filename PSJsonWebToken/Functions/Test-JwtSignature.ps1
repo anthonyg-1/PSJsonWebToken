2d936a9f4ab509e3dfbb4892c4104a510d2433ca
Tony Guimelli <tony.guimelli@gmail.com>
2021-04-12 14:33:15 -0400

    Update Test-JwtSignature.ps1
    Added JSON Web Key signature verification.

--- PSJsonWebToken/Functions/Test-JwtSignature.ps1
+++ PSJsonWebToken/Functions/Test-JwtSignature.ps1
@@ -13,6 +13,8 @@ function Test-JwtSignature
         The certificate that will be used to verify the signature of the JSON Web Token. The private key is NOT needed for signature verification.
     .PARAMETER Key
         This is the secret key used to generate an HMAC signature.
+    .PARAMETER JsonWebKey
+        The JSON Web Key (X509 certificate public key) to verify the signature of the JSON Web Token per RFC 7517.
     .EXAMPLE
         $jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI4QzVkRTlFMDNBNTQ0MDVBQkI2QmEyNDJENjI5MDU0MiIsImlhdCI6MTU5MjU4MTA4OSwiZXhwIjoxNTkyNTgxMzg5LCJzdWIiOiJtZUBjb21wYW55LmNvbSJ9.PkfNMxLIk0qaynr373qxgWR8lTNE5BLApFYhcG3TpK0"
         Test-JwtSignature -JsonWebToken $jwt -HashAlgorithm SHA256 -Key "secret"
@@ -24,12 +26,28 @@ function Test-JwtSignature
         Test-JwtSignature -JsonWebToken $jwt -HashAlgorithm SHA256 -VerificationCertificate $cert
 
         Verifies a digital signature for an RSA signed JSON Web Token against a certificate with thumbprint B31F009EEEEDDFAE34E977626E5A902600CF118C.
+    .EXAMPLE
+        $jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjJ5Q3Zabms3azhXNjZ3UjJMWFI5V0Nzd2hBYyIsImtpZCI6IjJ5Q3Zabms3azhXNjZ3UjJMWFI5V0Nzd2hBYyJ9.eyJpYXQiOjE2MTgyNTAzODksIm5iZiI6MTYxODI1MDM4OSwiZXhwIjoxNjE4MjU1MTg5LCJzdWIiOiJ0b255In0.X-RZm-3Hto5U-8Q-Wp1ggqWTFPkO5-Cz9lzoKsH5-1RR9GOrGPuWn-bjIv1YJ46h5Bw-KpiX-dOS47TAq2A0BWdAwczLVA6pzha1WswkT_u3cO1_KSoOjD9qFLjCgk-ns7A48iXpNcOoPBFXgfx8G0rRK68sSnokJ7N2NH-YNUOjg3U7DNJ_-iz8WZ5dNlOvpDsTy0BHMX-lho18sUmakUNpadJr-oD7BXIp--Z57UERBFibppaoxseYRo3VfmhgHibTxP-39mcxU6sH9a99fEEt80hj4w6rZobRxZV-pFPS22B8TBAfVf8L9faMLaXmgV7xtQohqQZgL6oKdJzFPQ"
+        $jwk = '
+        {
+            "kty": "RSA",
+            "use": "sig",
+            "e": "AQAB",
+            "n": "0yvTvlqT5yrk6lDzmK5_i6e-NKW4Bw8J9U62rcWI4IAr-vKaNqitmSwVLr2jJu29xQ__W22iGu584A82AS5N5YrwA6Rek-7WuHinwupFtCN-cCTzJlAcXUxyU7H0LfFxsXS1LUxSl7F_liIKH81QFE5RvI97R9bmbCn_BXpK4pHnTBGJigA8gJQ0U__YFk7AOSFUBeursQfCVPID99FpQ6pyj-h9WgdOneAfWde4SM1Pnovw59T2UT-JO-ObA5WOtvl0xW21djhhBRusVGWJuncNElhhRpUqNSOcsNQVe026zw8dX1wiMs9migQmz_LokH1bHENIuybdK9xBhXRRbw",
+            "kid": "2yCvZnk7k8W66wR2LXR9WCswhAc"
+        }
+        '
+        Test-JwtSignature -JsonWebToken $jwt -JsonWebKey $jwk -HashAlgorithm SHA256
+
+        Verifies a JSON Web Token's digital signature against a JSON Web Key.
     .OUTPUTS
         System.Boolean
     .LINK
         https://tools.ietf.org/html/rfc7515
+        https://tools.ietf.org/html/rfc7517
 		https://tools.ietf.org/html/rfc7519
         New-JwtSignature
+        New-JsonWebKeySet
 #>
 
     [CmdletBinding()]
@@ -43,9 +61,9 @@ function Test-JwtSignature
         [Parameter(Mandatory=$true,ParameterSetName="RSA",Position=2)][Alias("Certificate", "Cert")]
         [System.Security.Cryptography.X509Certificates.X509Certificate2]$VerificationCertificate,
 
-        [Parameter(Mandatory=$true,ParameterSetName="HMAC",Position=2)]
-        [ValidateNotNullOrEmpty()]
-        [String]$Key
+        [Parameter(Mandatory=$true,ParameterSetName="HMAC",Position=2)][ValidateLength(4,32768)][String]$Key,
+
+        [Parameter(Mandatory=$true,ParameterSetName="JWK",Position=2)][Alias("jwk")][ValidateLength(12, 1073741791)][String]$JsonWebKey
         )
 
         BEGIN
@@ -89,21 +107,33 @@ function Test-JwtSignature
                     $signatureVerifies = $true
                 }
             }
-            else
+            elseif ($PSCmdlet.ParameterSetName -eq "RSA")
             {
                 if ($null -ne $VerificationCertificate.PrivateKey.KeyExchangeAlgorithm)
-				{
-					Write-Warning -Message "It is not necessary to perform signature verification with a certificate that has private key!"
-				}
+                {
+                    Write-Warning -Message "It is not necessary to perform signature verification with a certificate that has private key!"
+                }
 
                 try
                 {
-                    $signatureVerifies = Test-JwtRsaSignature -JsonWebToken $JsonWebToken -VerificationCertificate $VerificationCertificate -HashAlgorithm $HashAlgorithm
+                    $signatureVerifies = Test-JwtRsaSignature -JsonWebToken $JsonWebToken -VerificationCertificate $VerificationCertificate -HashAlgorithm $HashAlgorithm -ErrorAction Stop
                 }
                 catch
                 {
-                    $signatureVerifies = $false
+                    Write-Error -Exception $_.Exception -ErrorAction Stop
                 }
+            }
+            else # ParameterSetName -eq JWK
+            {
+                try
+                {
+                    $signatureVerifies = Test-JwtJwkSignature -JsonWebToken $JsonWebToken -JsonWebKey $JsonWebKey -HashAlgorithm SHA256 -ErrorAction Stop
+                }
+                catch
+                {
+                    Write-Error -Exception $_.Exception -ErrorAction Stop
+                }
+
             }
 
             return $signatureVerifies
