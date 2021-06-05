49dfdd2f6da8fe8a1e41b1a491bf8672d24440ff
Tony Guimelli <tony.guimelli@gmail.com>
2021-06-05 14:29:29 -0400

    Update Get-JsonWebTokenSignature.ps1

--- PSJsonWebToken/Functions/Get-JsonWebTokenSignature.ps1
+++ PSJsonWebToken/Functions/Get-JsonWebTokenSignature.ps1
@@ -1,6 +1,5 @@
-function Get-JsonWebTokenSignature
-{
-<#
+function Get-JsonWebTokenSignature {
+    <#
     .SYNOPSIS
         Gets the JSON Web Token signature from the passed JWT.
     ..DESCRIPTION
@@ -11,7 +10,7 @@ function Get-JsonWebTokenSignature
         Returns the signature as a base 64 URL encoded string as opposed to the decoded value as a byte array.
     .EXAMPLE
         $jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.VG6H-orYnMLknmJajHx1HW9SftqCWeqE3TQ1UArx3Mk"
-        Get-JsonWebTokenSignature -JsonWebToken $jwt
+        $jwt | Get-JsonWebTokenSignature
 
         Returns the signature from the passed JWT as a byte array.
     .EXAMPLE
@@ -26,43 +25,39 @@ function Get-JsonWebTokenSignature
     .OUTPUTS
         System.Byte or System.String
     .LINK
-		New-JsonWebToken
+	New-JsonWebToken
         Test-JsonWebToken
         Get-JsonWebTokenHeader
         Get-JsonWebTokenPayload
 #>
     [CmdletBinding()]
+    [Alias('gjwtsig')]
     [OutputType([System.String], [System.Byte[]])]
     Param (
-        [Parameter(Mandatory=$true,ValueFromPipeline=$true,Position=0)]
-        [ValidateLength(16,8192)][Alias("JWT", "Token")][String]$JsonWebToken,
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
+        [ValidateLength(16, 8192)][Alias("JWT", "Token")][String]$JsonWebToken,
 
-        [Parameter(Mandatory=$false,ValueFromPipeline=$false,ValueFromPipelineByPropertyName=$false,Position=1)][Alias("AsIs")][switch]$AsEncodedString
+        [Parameter(Mandatory = $false, ValueFromPipeline = $false, ValueFromPipelineByPropertyName = $false, Position = 1)][Alias("AsIs")][switch]$AsEncodedString
     )
 
-    BEGIN
-    {
+    BEGIN {
         $decodeExceptionMessage = "Unable to decode JWT."
         $ArgumentException = New-Object -TypeName ArgumentException -ArgumentList $decodeExceptionMessage
     }
-    PROCESS
-    {
+    PROCESS {
         [bool]$isValidJwt = Test-JwtStructure -JsonWebToken $JsonWebToken -VerifySignaturePresent
-        if (-not($isValidJwt))
-        {
+        if (-not($isValidJwt)) {
             Write-Error -Exception $ArgumentException -Category InvalidArgument -ErrorAction Stop
         }
 
         $jwtSignature = $JsonWebToken.Split(".")[2]
 
-        if ($PSBoundParameters.ContainsKey("AsEncodedString"))
-        {
+        if ($PSBoundParameters.ContainsKey("AsEncodedString")) {
             return $jwtSignature
         }
-        else
-        {
+        else {
             $jwtSignatureByteArray = ConvertFrom-Base64UrlEncodedString -InputString $jwtSignature -AsBytes
             return $jwtSignatureByteArray
         }
-     }
+    }
 }
