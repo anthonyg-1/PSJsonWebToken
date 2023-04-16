eff6ae6c96d786307d3cf7d64e5e3cceb9998280
Tony Guimelli <tony.guimelli@gmail.com>
2023-04-16 13:53:35 -0400

    Create PemFromJwkResult.cs

---
+++ PSJsonWebToken/ClassDefinitions/PemFromJwkResult.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace PSJsonWebToken
+{
+    /// <summary>
+    /// Contains a public keys in PEM format converted from a JWK
+    /// </summary>
+    public class PemFromJwkResult
+    {
+        /// <summary>
+        /// The JWK ID as a string.
+        /// </summary>
+        public string JwkIdentifier { get; set; }
+
+        /// <summary>
+        /// The public key as a formatted PEM
+        /// </summary>
+        public string Pem { get; set; }
+
+        /// <summary>
+        /// The public key as an unformatted PEM
+        /// </summary>
+        public string PemUnformatted { get; set; }
+    }
+}
