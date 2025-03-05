f7392bbd47247c53ee929c50bbb91e8a4e1ef71d
Tony Guimelli <tony.guimelli@gmail.com>
2025-03-05 18:03:52 -0500

    Update Show-DecodedJwt.ps1

--- PSJsonWebToken/Functions/Show-DecodedJwt.ps1
+++ PSJsonWebToken/Functions/Show-DecodedJwt.ps1
@@ -6,17 +6,24 @@ function Show-DecodedJwt {
          Displays a JSON Web Token header and payload in color for easy viewing. Note that this advanced function is not meant to send any data and/or objects down the pipeline.
     .PARAMETER JsonWebToken
         The JSON Web Token to be decoded and displayed.
+    .PARAMETER SendToClipboard
+        Tells the function to send the decoded JSON Web Token to the clipboard.
     .EXAMPLE
         $jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjJ5Q3Zabms3azhXNjZ3UjJMWFI5V0Nzd2hBYyIsImtpZCI6IjJ5Q3Zabms3azhXNjZ3UjJMWFI5V0Nzd2hBYyJ9.eyJpYXQiOjE2MTgyNTAzODksIm5iZiI6MTYxODI1MDM4OSwiZXhwIjoxNjE4MjU1MTg5LCJzdWIiOiJ0b255In0.X-RZm-3Hto5U-8Q-Wp1ggqWTFPkO5-Cz9lzoKsH5-1RR9GOrGPuWn-bjIv1YJ46h5Bw-KpiX-dOS47TAq2A0BWdAwczLVA6pzha1WswkT_u3cO1_KSoOjD9qFLjCgk-ns7A48iXpNcOoPBFXgfx8G0rRK68sSnokJ7N2NH-YNUOjg3U7DNJ_-iz8WZ5dNlOvpDsTy0BHMX-lho18sUmakUNpadJr-oD7BXIp--Z57UERBFibppaoxseYRo3VfmhgHibTxP-39mcxU6sH9a99fEEt80hj4w6rZobRxZV-pFPS22B8TBAfVf8L9faMLaXmgV7xtQohqQZgL6oKdJzFPQ"
         $jwt | Show-DecodedJwt
 
         Displays a decoded JSON Web Token with the header in red, the body in cyan/blue, and a green placeholder for the signature.
+  .EXAMPLE
+        $jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjJ5Q3Zabms3azhXNjZ3UjJMWFI5V0Nzd2hBYyIsImtpZCI6IjJ5Q3Zabms3azhXNjZ3UjJMWFI5V0Nzd2hBYyJ9.eyJpYXQiOjE2MTgyNTAzODksIm5iZiI6MTYxODI1MDM4OSwiZXhwIjoxNjE4MjU1MTg5LCJzdWIiOiJ0b255In0.X-RZm-3Hto5U-8Q-Wp1ggqWTFPkO5-Cz9lzoKsH5-1RR9GOrGPuWn-bjIv1YJ46h5Bw-KpiX-dOS47TAq2A0BWdAwczLVA6pzha1WswkT_u3cO1_KSoOjD9qFLjCgk-ns7A48iXpNcOoPBFXgfx8G0rRK68sSnokJ7N2NH-YNUOjg3U7DNJ_-iz8WZ5dNlOvpDsTy0BHMX-lho18sUmakUNpadJr-oD7BXIp--Z57UERBFibppaoxseYRo3VfmhgHibTxP-39mcxU6sH9a99fEEt80hj4w6rZobRxZV-pFPS22B8TBAfVf8L9faMLaXmgV7xtQohqQZgL6oKdJzFPQ"
+        $jwt | Show-DecodedJwt -SendToClipboard
+
+        Displays a decoded JSON Web Token with the header in red, the body in cyan/blue, and a green placeholder for the signature as well as sending the decoded JSON Web Token to the clipboard.
     .INPUTS
         System.String
-        A string is received by the JsonWebToken parameter.
+            A string is received by the JsonWebToken parameter.
     .OUTPUTS
         None
-        This cmdlet returns no output. It sends the objects to the host. The host displays the objects this cmdlet sends to it.
+            This cmdlet returns no output. It sends text output to the host and optionally the clipboard.
     .LINK
         https://tools.ietf.org/html/rfc7519
 #>
@@ -25,7 +32,10 @@ function Show-DecodedJwt {
     [OutputType([System.Boolean])]
     Param (
         [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
-        [ValidateLength(16, 131072)][Alias("JWT", "Token")][String]$JsonWebToken
+        [ValidateLength(16, 131072)][Alias("JWT", "Token")][String]$JsonWebToken,
+
+        [Parameter(Mandatory = $false, ValueFromPipeline = $false, Position = 1,
+            ValueFromPipelineByPropertyName = $false)][Alias("clip", "ToClipboard", "c")][Switch]$SendToClipboard
     )
     BEGIN {
         $decodeExceptionMessage = "Unable to decode JWT."
@@ -49,14 +59,31 @@ function Show-DecodedJwt {
         $header = Get-JsonWebTokenHeader -JsonWebToken $JsonWebToken
         $payload = Get-JsonWebTokenPayload -JsonWebToken $JsonWebToken
 
+        $headerJson = $header | ConvertTo-Json -Depth 25
+        $payloadJson = $payload | ConvertTo-Json -Depth 25
+        $signaturePlaceholderString = "[Signature]"
+
+        if ($PSBoundParameters.ContainsKey("SendToClipboard")) {
+            [string]$jwtStringForClipboard = ""
+
+            if ((($JsonWebToken.Split(".")[2]).Length -gt 8)) {
+                $jwtStringForClipboard = "{0}.{1}.{2}" -f $headerJson, $payloadJson, $signaturePlaceholderString
+            }
+            else {
+                $jwtStringForClipboard = "{0}.{1}" -f $headerJson, $payloadJson
+            }
+
+            $jwtStringForClipboard | Set-Clipboard
+        }
+
         # Serialize the hashtables into JSON and output via Write-Host
         Write-Host -Object ""
-        $header | ConvertTo-Json -Depth 25 | Write-Host -ForegroundColor Red -NoNewline
+        $headerJson | Write-Host -ForegroundColor Red -NoNewline
         Write-Host -Object "." -ForegroundColor Yellow -NoNewline
-        $payload | ConvertTo-Json -Depth 25 | Write-Host -ForegroundColor Cyan -NoNewline
+        $payloadJson | Write-Host -ForegroundColor Cyan -NoNewline
         Write-Host -Object "." -ForegroundColor Yellow -NoNewline
         if ((($JsonWebToken.Split(".")[2]).Length -gt 8)) {
-            Write-Host -Object "[Signature]" -ForegroundColor Green
+            Write-Host -Object $signaturePlaceholderString -ForegroundColor Green
         }
         Write-Host -Object ""
     }
